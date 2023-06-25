f902282414fa89a4d7c3c39fbdd4fc84bcca9fb8
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-06-25 17:56:02 +0700

    feat: update cart page

--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -1,25 +1,35 @@
 'use client';
 
 import { useSelector } from 'react-redux';
+import { notFound } from 'next/navigation';
 import { selectTotalAmount } from '../../redux/features/cart/selector';
 import { RootState } from '../../redux/store';
 import { selectMovies } from '../../redux/features/movies/selector';
+import { useGetMoviesQuery } from '../../redux/services/movieApi';
 import { NotFound } from '../../components/NotFound/NotFound';
 import { FilmCard } from '../../components/FilmCard/FilmCard';
+import { Loading } from '../../components/Loading/Loading';
 import { localizeGenre } from '../../lib/utils';
 
 import styles from './styles.module.css';
 import classnames from 'classnames';
 
 export default function Cart() {
+  const { data, isLoading, error } = useGetMoviesQuery();
   const cartState = useSelector((state: RootState) => state.cart);
   const cartCount = useSelector((state: RootState) => selectTotalAmount(state));
-  const films = useSelector((state: RootState) => selectMovies(state));
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (!data || error) {
+    return notFound();
+  }
 
   const filmsInCart = Object.keys(cartState)
-    .map((id) => films.find((film) => film.id === id))
+    .map((id) => data.find((film) => film.id === id))
     .filter((movie) => movie !== undefined);
-  console.log(filmsInCart);
 
   return (
     <div className={styles.cart}>
