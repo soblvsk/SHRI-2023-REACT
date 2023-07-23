481712b8a82ec6d88f99773e9b79e02a9416f3d3
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-23 11:33:35 +0700

    feat: delete lib folder

--- frontend/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-export const localizeGenre = (genre: string) => {
-  const genreLocalization: { [key in string]: string } = {
-    action: 'Экшен',
-    horror: 'Ужасы',
-    comedy: 'Комедия',
-    fantasy: 'Фэнтези',
-  };
-  return genreLocalization[genre];
-};
