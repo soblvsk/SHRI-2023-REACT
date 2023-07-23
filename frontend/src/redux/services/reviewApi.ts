10d41858e4b8ae7e8e903ba9402d2151163c5763
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-23 11:31:14 +0700

    feat: change reviewApi

--- frontend/src/redux/services/reviewApi.ts
+++ frontend/src/redux/services/reviewApi.ts
@@ -1,13 +1,16 @@
-import { Review } from '@/lib/interfaces';
+import { Review } from '@/constants/interfaces';
+
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const reviewApi = createApi({
-  reducerPath: 'reviewApi',
+  reducerPath: 'reviewAPi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/api/' }),
   endpoints: (builder) => ({
     getReviews: builder.query<Review[], void>({ query: () => 'reviews' }),
-    getReview: builder.query<Review[], string>({ query: (movieId) => `reviews?movieId=${movieId}` }),
+    getReviewsByMovie: builder.query<Review[], void>({
+      query: (movieId) => `reviews?movieId=${movieId}`,
+    }),
   }),
 });
 
-export const { useGetReviewsQuery, useGetReviewQuery } = reviewApi;
+export const { useGetReviewsQuery, useGetReviewsByMovieQuery } = reviewApi;
