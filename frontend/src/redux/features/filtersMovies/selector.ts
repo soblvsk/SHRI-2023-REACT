589c896339999312113c4f6986f6c698bbd0b774
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-23 11:32:19 +0700

    feat: delete feature filtersMovie

--- frontend/src/redux/features/filtersMovies/selector.ts
+++
@@ -1,4 +0,0 @@
-import { RootState } from '@/redux/store';
-const selectMoviesModule = (state: RootState) => state.filteredMovie;
-
-export const selectFilteredMovies = (state: RootState) => selectMoviesModule(state) || [];
