709cd7082f5fb7623282acbe0c7d49354af823c6
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-23 11:31:34 +0700

    feat: change cinemaApi

--- frontend/src/redux/services/cinemaApi.ts
+++ frontend/src/redux/services/cinemaApi.ts
@@ -1,11 +1,26 @@
-import { Cinema } from '@/lib/interfaces';
+import { Cinema } from '@/constants/interfaces';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const cinemaApi = createApi({
-  reducerPath: 'cinemaApi',
+  reducerPath: 'cinemaAPi',
+  tagTypes: ['Cinema'],
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/api/' }),
   endpoints: (builder) => ({
-    getCinemas: builder.query<Cinema[], void>({ query: () => 'cinemas' }),
+    getCinemas: builder.query<Cinema[], void>({
+      query: () => ({ url: 'cinemas' }),
+      providesTags: (result) =>
+        result
+          ? result.map(({ id }) => ({ id, type: 'Cinema' as const })).concat([{ type: 'Cinema', id: 'LIST' }])
+          : [{ type: 'Cinema', id: 'LIST' }],
+    }),
+    updateCinemas: builder.mutation<Cinema, void>({
+      query: () => ({ url: '', method: 'PATCH' }),
+      invalidatesTags: (result) => (result ? [{ type: 'Cinema', id: result.id }] : []),
+    }),
+    createCinemas: builder.mutation<Cinema, void>({
+      query: () => ({ url: '', method: 'POST' }),
+      invalidatesTags: () => [{ type: 'Cinema', id: 'LIST' }],
+    }),
   }),
 });
 
