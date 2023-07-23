4982c7ac19a27e96e48a9211915a46b96f76fdbb
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-23 11:32:39 +0700

    feat: change selectors feature cart

---
+++ frontend/src/redux/features/cart/selectors.ts
@@ -0,0 +1,8 @@
+import { RootState } from '@/redux/store';
+
+export const selectCartModule = (state: RootState) => state.cart;
+
+export const selectMovieAmount = (state: RootState, movieId: string) => selectCartModule(state)[movieId] || 0;
+
+export const selectMoviesAmount = (state: RootState) =>
+  Object.values(selectCartModule(state)).reduce((acc, amount) => acc + amount, 0);
