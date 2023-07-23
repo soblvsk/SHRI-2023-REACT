55b76d616f9424ad01c4e8da0684657d79814f8e
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-23 11:33:03 +0700

    feat: update feature cart

--- frontend/src/redux/features/cart/index.ts
+++ frontend/src/redux/features/cart/index.ts
@@ -6,33 +6,53 @@ interface CartState {
 
 const initialState: CartState = {};
 
-const cartSlice = createSlice({
+export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
     increment: (state, { payload }) => {
-      const count = state[payload] || 0;
-      state[payload] = count + 1;
+      state[payload] = state[payload] ? state[payload] + 1 : 1;
     },
     decrement: (state, { payload }) => {
-      const count = state[payload];
-
-      if (!count) {
-        return;
-      }
-
-      if (count === 1) {
-        delete state[payload];
-        return;
-      }
-
-      state[payload] = count - 1;
-    },
-    reset: (state, { payload }) => {
-      delete state[payload];
+      state[payload] = state[payload] ? state[payload] - 1 : 0;
     },
   },
 });
+// import { createSlice } from '@reduxjs/toolkit';
 
-export const cartReducer = cartSlice.reducer;
-export const cartActions = cartSlice.actions;
+// interface CartState {
+//   [key: string]: number;
+// }
+
+// const initialState: CartState = {};
+
+// const cartSlice = createSlice({
+//   name: 'cart',
+//   initialState,
+//   reducers: {
+//     increment: (state, { payload }) => {
+//       const count = state[payload] || 0;
+//       state[payload] = count + 1;
+//     },
+//     decrement: (state, { payload }) => {
+//       const count = state[payload];
+
+//       if (!count) {
+//         return;
+//       }
+
+//       if (count === 1) {
+//         delete state[payload];
+//         return;
+//       }
+
+//       state[payload] = count - 1;
+//     },
+//     reset: (state, { payload }) => {
+//       delete state[payload];
+//     },
+//   },
+// });
+
+// export const cartReducer = cartSlice.reducer;
+// export const cartActions = cartSlice.actions;
