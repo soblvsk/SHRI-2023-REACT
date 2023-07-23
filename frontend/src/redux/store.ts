f1c7ca7e544e4ce630da7522390bc19ed17956b5
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-23 11:33:15 +0700

    feat: change store redux

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,21 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { cartReducer } from '@/redux/features/cart';
-import { filteredMoviesReducer } from '@/redux/features/filtersMovies';
-import { logger } from '@/redux/middleware/logger';
+import { cinemaApi } from '@/redux/services/cinemaApi';
 import { movieApi } from '@/redux/services/movieApi';
 import { reviewApi } from '@/redux/services/reviewApi';
-import { cinemaApi } from '@/redux/services/cinemaApi';
+import { cartSlice } from '@/redux/features/cart';
 
 export const store = configureStore({
   reducer: {
-    filteredMovie: filteredMoviesReducer,
-    cart: cartReducer,
-    [movieApi.reducerPath]: movieApi.reducer,
     [cinemaApi.reducerPath]: cinemaApi.reducer,
+    [movieApi.reducerPath]: movieApi.reducer,
     [reviewApi.reducerPath]: reviewApi.reducer,
+    cart: cartSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([movieApi.middleware, cinemaApi.middleware, reviewApi.middleware, logger]),
+    getDefaultMiddleware().concat([cinemaApi.middleware, movieApi.middleware, reviewApi.middleware]),
   devTools: process.env.NODE_ENV !== 'production',
 });
 
