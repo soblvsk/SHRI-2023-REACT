74572f6e541da3b624f2d4951a26fce2c9373fd0
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-24 13:02:09 +0700

    feat: add pagesComponent Cart

---
+++ frontend/src/pagesComponents/Cart/component.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react';
+
+interface Props {
+  movieId: string;
+}
+
+export const CartPage: FunctionComponent<Props> = ({ movieId }) => {
+  return <div></div>;
+};
