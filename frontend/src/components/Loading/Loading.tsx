4a407a3563a515031b5353d265b66739b97948db
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-24 13:03:52 +0700

    feat: delete component Loading

--- frontend/src/components/Loading/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.css';
-
-export const Loading = () => {
-  return (
-    <div className={styles.loading}>
-      <div className={styles['loading__spinner']}></div>
-    </div>
-  );
-};
