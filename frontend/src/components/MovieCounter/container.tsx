eea62a224ccca7d754e1ce87a164931201cf5d91
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-27 10:10:04 +0700

    feat: change component movieCounter container

--- frontend/src/components/MovieCounter/container.tsx
+++ frontend/src/components/MovieCounter/container.tsx
@@ -1,17 +1,13 @@
 'use client';
 
-import { RootState } from '@/redux/store';
-import { MovieCounter } from '@/components/MovieCounter/component';
-import { cartSlice } from '@/redux/features/cart';
-import { selectMovieAmount } from '@/redux/features/cart/selectors';
 import React, { FunctionComponent } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import { cartSlice } from '@/redux/features/cart';
+import { selectMovieAmount } from '@/redux/features/cart/selectors';
+import { MovieCounter } from '@/components/MovieCounter/component';
 
-interface Props {
-  movieId: string;
-}
-
-export const MovieCounterContainer: FunctionComponent<Props> = ({ movieId }) => {
+export const MovieCounterContainer: FunctionComponent<{ movieId: string }> = ({ movieId }) => {
   const amount = useSelector((state: RootState) => selectMovieAmount(state, movieId));
   const dispatch = useDispatch();
 
