98096441b51407be40a40744682bf95ddfe240db
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-28 10:22:39 +0700

    feat: update reviewApi redux

--- frontend/src/redux/services/reviewApi.ts
+++ frontend/src/redux/services/reviewApi.ts
@@ -1,13 +1,12 @@
-import { Review } from '@/constants/interfaces';
-
+import { FeedBack } from '@/constants/interfaces';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const reviewApi = createApi({
   reducerPath: 'reviewAPi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/api/' }),
   endpoints: (builder) => ({
-    getReviews: builder.query<Review[], void>({ query: () => 'reviews' }),
-    getReviewsByMovie: builder.query<Review[], void>({
+    getReviews: builder.query<FeedBack[], void>({ query: () => 'reviews' }),
+    getReviewsByMovie: builder.query<FeedBack[], void>({
       query: (movieId) => `reviews?movieId=${movieId}`,
     }),
   }),
