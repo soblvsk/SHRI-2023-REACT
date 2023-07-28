b9ab1c7c805763a036b1815b4f591853d853e9c5
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-28 10:24:06 +0700

    feat: update movies component

--- frontend/src/pagesComponents/Movies/component.tsx
+++ frontend/src/pagesComponents/Movies/component.tsx
@@ -1,14 +1,10 @@
 import React, { FunctionComponent } from 'react';
-
-import styles from './styles.module.scss';
 import { MovieFiltersContainer } from '@/components/MovieFilters/container';
 import { MoviesContainer } from '@/components/Movies/container';
 
-interface Props {
-  cinemaId: string;
-}
+import styles from './styles.module.scss';
 
-export const MoviesPage: FunctionComponent<Props> = ({ cinemaId }) => {
+export const MoviesPage: FunctionComponent<{ cinemaId: string }> = ({ cinemaId }) => {
   return (
     <div className={styles.root}>
       <div className={styles.filters}>
