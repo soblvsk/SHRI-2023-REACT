af104651de7024a1933eebab68fe89a15902f174
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-28 10:22:21 +0700

    feat: update api services

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,25 +1,25 @@
-import { Cinema, Movie, Review } from '@/constants/interfaces';
+import { Cinema, Film, FeedBack } from '@/constants/interfaces';
 
 export async function fetchCinemas(): Promise<Cinema[]> {
   const response = await fetch('http://localhost:3001/api/cinemas');
   return await response.json();
 }
 
-export async function fetchMovies(cinemaId: string): Promise<Movie[]> {
+export async function fetchMovies(cinemaId: string): Promise<Film[]> {
   const response = await fetch(`http://localhost:3001/api/movies?cinemaId=${cinemaId}`, {
     next: { tags: ['top10'] },
   });
   return await response.json();
 }
 
-export async function fetchMovie(movieId: string): Promise<Movie> {
+export async function fetchMovie(movieId: string): Promise<Film> {
   const response = await fetch(`http://localhost:3001/api/movie?movieId=${movieId}`, {
     next: { tags: [`movie-${movieId}`] },
   });
   return await response.json();
 }
 
-export async function fetchMovieReviews(movieId: string): Promise<Review> {
+export async function fetchMovieReviews(movieId: string): Promise<FeedBack[]> {
   const response = await fetch(`http://localhost:3001/api/reviews?movieId=${movieId}`);
   return await response.json();
 }
