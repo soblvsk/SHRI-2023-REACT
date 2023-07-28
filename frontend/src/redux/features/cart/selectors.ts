0bed38db27af4c73697fad2abcb0dca9d2e57ea1
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-28 10:23:00 +0700

    feat: update selectors redux

--- frontend/src/redux/features/cart/selectors.ts
+++ frontend/src/redux/features/cart/selectors.ts
@@ -5,4 +5,4 @@ export const selectCartModule = (state: RootState) => state.cart;
 export const selectMovieAmount = (state: RootState, movieId: string) => selectCartModule(state)[movieId] || 0;
 
 export const selectMoviesAmount = (state: RootState) =>
-  Object.values(selectCartModule(state)).reduce((acc, amount) => acc + amount, 0);
+  Object.values(selectCartModule(state)).reduce((acc: number, amount: number) => acc + amount, 0);
