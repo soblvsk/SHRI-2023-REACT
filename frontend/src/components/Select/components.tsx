fabc8d88adf4e0e6787990698ed7a5a98fdc4324
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-29 12:01:49 +0700

    feat: change component Select

--- frontend/src/components/Select/components.tsx
+++ frontend/src/components/Select/components.tsx
@@ -2,6 +2,7 @@ import { SelectPortal } from '@/components/SelectPortal/component';
 import React, { FunctionComponent, useEffect, useRef, useState } from 'react';
 
 import styles from './styles.module.scss';
+import classNames from 'classnames';
 
 interface Option {
   key: string;
@@ -38,6 +39,20 @@ export const Select: FunctionComponent<{
     <div>
       <button onClick={() => setIsOpened(!isOpened)} ref={selectRef} className={styles.select}>
         {options.find(({ key }) => key === value)?.title || placeHolder}
+        <svg
+          className={classNames(styles.select__icon, isOpened ? styles.select__iconOpen : '')}
+          xmlns='http://www.w3.org/2000/svg'
+          width='20'
+          height='20'
+          fill='none'
+        >
+          <path
+            fill='#999FA6'
+            fillRule='evenodd'
+            d='M7.5 18.958h5c4.525 0 6.458-1.933 6.458-6.458v-5c0-4.525-1.933-6.458-6.458-6.458h-5c-4.525 0-6.458 1.933-6.458 6.458v5c0 4.525 1.933 6.458 6.458 6.458ZM2.292 7.5c0-3.842 1.366-5.208 5.208-5.208h5c3.842 0 5.208 1.366 5.208 5.208v5c0 3.842-1.366 5.208-5.208 5.208h-5c-3.842 0-5.208-1.366-5.208-5.208v-5Zm7.266 4.742a.618.618 0 0 0 .442.183.618.618 0 0 0 .442-.183L13.383 9.3a.629.629 0 0 0 0-.883.629.629 0 0 0-.883 0l-2.5 2.5-2.5-2.5a.629.629 0 0 0-.883 0 .629.629 0 0 0 0 .883l2.941 2.942Z'
+            clipRule='evenodd'
+          />
+        </svg>
       </button>
       <SelectPortal visible={isOpened} onClose={() => setIsOpened(false)}>
         <div
