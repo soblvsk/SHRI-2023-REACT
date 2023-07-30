677b7c9c43f63ef1860e07bc717a81e7e1c894d8
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-30 19:24:52 +0700

    feat: change page cart

--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -1,60 +1,6 @@
-'use client';
-
-import { useSelector } from 'react-redux';
-import { notFound } from 'next/navigation';
-import { selectTotalAmount } from '../../redux/features/cart/selector';
-import { RootState } from '../../redux/store';
-import { useGetMoviesQuery } from '../../redux/services/movieApi';
-import { NotFound } from '../../components/NotFound/NotFound';
-import { FilmCard } from '../../components/FilmCard/FilmCard';
-import { Loading } from '../../components/Loading/Loading';
-import { localizeGenre } from '../../lib/utils';
-
-import styles from './styles.module.css';
-import classnames from 'classnames';
+import { CartPage } from '@/pagesComponents/Cart/component';
 
 export default function Cart() {
-  const { data, isLoading, error } = useGetMoviesQuery();
-  const cartState = useSelector((state: RootState) => state.cart);
-  const cartCount = useSelector((state: RootState) => selectTotalAmount(state));
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  if (!data || error) {
-    return notFound();
-  }
-
-  const filmsInCart = Object.keys(cartState)
-    .map((id) => data.find((film) => film.id === id))
-    .filter((movie) => movie !== undefined);
-
-  return (
-    <div className={styles.cart}>
-      <div className={styles['cart__items']}>
-        {filmsInCart.length > 0 ? (
-          filmsInCart.map(
-            (movie) =>
-              movie && (
-                <FilmCard
-                  key={movie.id}
-                  id={movie.id}
-                  title={movie.title}
-                  posterUrl={movie.posterUrl}
-                  genre={localizeGenre(movie.genre)}
-                  isCart={true}
-                />
-              ),
-          )
-        ) : (
-          <NotFound />
-        )}
-      </div>
-      <div className={classnames('font-semibold', styles['cart__result'])}>
-        <span>Итого билетов:</span>
-        <span>{cartCount}</span>
-      </div>
-    </div>
-  );
+  return <></>;
+  // return <CartPage />;
 }
