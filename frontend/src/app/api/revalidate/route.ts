db13c24720dbd9066e14a5305383440574d29747
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-07-31 11:31:35 +0700

    feat: add route api

---
+++ frontend/src/app/api/revalidate/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { revalidateTag } from 'next/cache';
+
+export async function GET(request: NextRequest) {
+  const tag = request.nextUrl.searchParams.get('tag');
+
+  if (tag) {
+    revalidateTag(tag);
+    return NextResponse.json({ revalidated: true });
+  }
+
+  return NextResponse.json({ revalidated: false });
+}
