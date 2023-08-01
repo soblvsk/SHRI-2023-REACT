7d65969706f06ab7e015750920e6996e79529cff
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-08-01 19:21:09 +0700

    feat: change selectors redux

--- frontend/src/redux/features/cart/selectors.ts
+++ frontend/src/redux/features/cart/selectors.ts
@@ -2,7 +2,9 @@ import { RootState } from '@/redux/store';
 
 export const selectCartModule = (state: RootState) => state.cart;
 
-export const selectMovieAmount = (state: RootState, movieId: string) => selectCartModule(state)[movieId] || 0;
+export const selectMovieAmount = (state: RootState, id: string) => selectCartModule(state)[id] || 0;
 
-export const selectMoviesAmount = (state: RootState) =>
-  Object.values(selectCartModule(state)).reduce((acc: number, amount: number) => acc + amount, 0);
+export const selectTotalAmount = (state: RootState) => {
+  const cart = selectCartModule(state);
+  return Object.values(cart).reduce((sum, amount) => sum + amount, 0);
+};
