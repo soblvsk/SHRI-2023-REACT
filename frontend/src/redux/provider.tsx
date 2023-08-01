541a5845367dad24498edabc1e5dbafd08f7bfd1
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-08-01 19:21:52 +0700

    feat: change provider redux

--- frontend/src/redux/provider.tsx
+++ frontend/src/redux/provider.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { store } from './store';
 import React, { FunctionComponent } from 'react';
 import { Provider } from 'react-redux';
+import { store } from './store';
 
 export const StoreProvider: FunctionComponent<{ children: React.ReactNode }> = ({ children }) => {
   return <Provider store={store}>{children} </Provider>;
