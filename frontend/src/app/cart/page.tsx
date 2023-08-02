7d320aafd34a718f7f0406348b1daf28776f4a42
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-08-02 12:47:29 +0700

    feat: change page cart

--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -1,6 +1,10 @@
+import { Metadata } from 'next';
 import { CartPage } from '@/pagesComponents/Cart/component';
 
+export const metadata: Metadata = {
+  title: 'Корзина',
+};
+
 export default function Cart() {
-  return <></>;
-  // return <CartPage />;
+  return <CartPage />;
 }
